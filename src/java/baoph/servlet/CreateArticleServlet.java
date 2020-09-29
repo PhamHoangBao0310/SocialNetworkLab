192dfa1123c3973e8d4910f15552ca0be36b9f36
PhamHoangBao0310 <baophse141135@fpt.edu.vn>
2020-09-29 13:36:02 +0700

    test

--- src/java/baoph/servlet/CreateArticleServlet.java
+++ src/java/baoph/servlet/CreateArticleServlet.java
@@ -81,10 +81,10 @@ public class CreateArticleServlet extends HttpServlet {
                         index++;
                         // add image link to save in database
                         imageLink = imageLink + "image/" + filename + " ";
-                        System.out.println("path : " + filename);
+//                        System.out.println("path : " + filename);
                         // create image path to write image
                         String realPath = getServletContext().getRealPath("/") + "image\\" + filename;
-                        System.out.println("RPath " + realPath);
+//                        System.out.println("RPath " + realPath);
                         // Write image by inputstream and outputstream
                         try (InputStream fileInput = part.getInputStream()) {
                             File f = new File(realPath);
